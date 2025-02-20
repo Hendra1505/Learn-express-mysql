b8ff4bd72ed972d19a8990bd7f509036d05b07cb
Hendra1505 <hendraekky1946@gmail.com>
2025-02-20 21:46:33 +0700

    [Ekky] Handling routes in express js

--- section5:working_with_express_js/app.js
+++ section5:working_with_express_js/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('In the middleware');
-    next();
+    res.send('<h1>Handling route "Add Product" Page </h1>');
 })
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('In another middleware');
-    res.send('<h1>Testing function bawaan dari express js</h1>')
+    res.send('<h1>Testing function bawaan dari express js</h1>');
 })
 
 // const server = http.createServer();
