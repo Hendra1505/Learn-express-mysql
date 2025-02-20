7acfda15525915e915f8322dc3fc082d87c204fe
Hendra1505 <hendraekky1946@gmail.com>
2025-02-20 21:37:18 +0700

    [Ekky] Use function in express js

--- section5:working_with_express_js/app.js
+++ section5:working_with_express_js/app.js
@@ -1,9 +1,17 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer();
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next();
+})
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Testing function bawaan dari express js</h1>')
+})
+
+// const server = http.createServer();
+app.listen(3000);
+// server.listen(3000);
